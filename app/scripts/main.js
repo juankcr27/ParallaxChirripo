f185afb3892c13c164dda556d421fe108753e0bb
bryancr89 <bazofeifa89@gmail.com>
2014-01-09 17:05:45 -0600

    Fixed issues with Parallax

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,34 @@
 'use strict';
 
+function goToByScroll(id){
+	var r = /\d+/,
+		$id = $("#"+id),
+		offsetTop = $id.offset().top;
+	$('html,body').animate({
+			scrollTop: offsetTop// - $id.height() /2 + top
+		},
+		'slow');
+}
+
 $( document ).ready(function() {
+	var lastId = '';
+
+
+	$( 'nav a' ).each(function() {
+		var id = $(this).data('id');
+		$(this).click(function( e ) {
+			e.preventDefault();
+			if(lastId !== id) {
+				lastId = id;
+				goToByScroll(id);
+			}
+		});
+	});
+
 	$( window ).stellar({
 		verticalScrolling: true,
-		 hideDistantElements: false
+		hideDistantElements: false
 	});
+
+	$(this).scrollTop(0);
 });
